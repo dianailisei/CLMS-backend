296303579bbd059aa657579a55e6e910c838877b
Codrut Iftimie <CodrutIftimie@users.noreply.github.com>
2018-12-04 23:33:09 +0200

    Entities
    - Subject
    - Lecture
    - Laboratory
    - Course

---
+++ Schedule.Domain/Entities/Lecture.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Schedule.Domain.Entities
+{
+    class Lecture : Course
+    {
+        public string HalfYear { get; private set; }
+        public ICollection<Teacher> Teachers { get; private set; }
+
+        public static Lecture Create(string name, string weekday, short starthour, short endhour) => new Lecture
+        {
+            Id = new Guid(),
+            Name = name,
+            Weekday = weekday,
+            StartHour = starthour,
+            EndHour = endhour
+        };
+    }
+}
