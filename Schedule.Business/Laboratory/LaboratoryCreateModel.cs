c2d3b8622223d377bff9bf41bbf4ee2c46c67e48
Diana Ilisei <dianailisei@gmail.com>
2018-12-06 11:44:48 +0200

    Laboratory Service, Controller

---
+++ Schedule.Business/Laboratory/LaboratoryCreateModel.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Schedule.Domain.Entities;
+
+namespace Schedule.Business.Laboratory
+{
+    public class LaboratoryCreateModel
+    {
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Group { get; set; }
+
+        [Required]
+        public Teacher Teacher { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Weekday { get; set; }
+
+        [Required]
+        public short StartHour { get; set; }
+
+        [Required]
+        public short EndHour { get; set; }
+    }
+}
--- Schedule.Domain/Entities/Person.cs
+++ Schedule.Domain/Entities/Person.cs
@@ -2,9 +2,9 @@
 {
     public class Person : Entity
     {
-        public string FirstName { get; private set; }
-        public string LastName { get; private set; }
-        public string Email { get; private set; }
-        public string Password { get; private set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
     }
 }
